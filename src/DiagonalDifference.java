b52be6bf3500d89a3a64c12700266d07a1be7a7a
TK Lee <tklee1127@gmail.com>
2019-07-25 23:12:54 -0400

    Hacker Rank Algorithm Question - Diagonal Difference Solution

---
+++ src/DiagonalDifference.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class DiagonalDifference {
+
+    /*
+     * Hacker Rank Algorithm Question - Diagonal Difference Solution
+     *
+     * Solved Date : 2019-07-25
+     * Author : TK Lee
+     *
+     * Source : https://www.hackerrank.com/challenges/diagonal-difference/problem
+     */
+
+    public static void main (String[] args){
+
+        int left = 0;
+        int right = 0;
+        int val = 0;
+        Scanner sc = new Scanner(System.in);
+        
+        int rowNum = sc.nextInt();
+
+        //Test Cases
+        //int[][] matrix =  {{11, 2, 4}, {4,5,6}, {10, 8, -12}};
+
+        for (int row = 0 ; row<rowNum; row++){
+            for(int col = 0; col<rowNum; col++){
+
+              val = sc.nextInt();
+              // val = matrix[row][col];
+               
+               if(row == col){
+                    left += val;
+                }
+               if(col == rowNum - (1 + row)){
+                    right += val;
+                }
+            }
+        }
+
+        System.out.println (Math.abs(left - right));
+    }
+}
