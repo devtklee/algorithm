84d2b0ba71dbc8ff2cf390b53e683526abd386dc
TK Lee <tklee1127@gmail.com>
2019-08-08 16:38:51 -0400

    HackersRank Algorithm Study Solution - Arrays DS

---
+++ src/dataStructureAlgorithmQuestions/Arrays.java
@@ -0,0 +1,66 @@
+package dataStructureAlgorithmQuestions;
+
+import java.io.*;
+import java.util.*;
+
+public class Arrays {
+
+    /*
+     * Hacker Rank Algorithm Question - Arrays - DS Solution
+     * *
+     * Category : Data Structure
+     * Solved Date : 2019-08-08
+     * Author : TK Lee
+     *
+     * Source : https://www.hackerrank.com/challenges/arrays-ds/problem
+     */
+
+    // Complete the reverseArray function below.
+    static int[] reverseArray(int[] a) {
+
+        int[] ra = new int [a.length];
+        int cnt = 0;
+
+        for (int idx = a.length-1; idx >= 0;  idx--){
+
+            ra[idx] = a[cnt++];
+        }
+
+        return ra;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[arrCount];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arrCount; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] res = reverseArray(arr);
+
+        for (int i = 0; i < res.length; i++) {
+            bufferedWriter.write(String.valueOf(res[i]));
+
+            if (i != res.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
