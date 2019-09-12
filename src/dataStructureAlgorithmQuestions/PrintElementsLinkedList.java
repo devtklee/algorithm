aa7c37eeac003f2c9c3f00216e0ae0053c82ce5d
TK Lee <tklee1127@gmail.com>
2019-09-12 03:25:03 -0400

    Print the Elements of a Linked List Solution

---
+++ src/dataStructureAlgorithmQuestions/PrintElementsLinkedList.java
@@ -0,0 +1,91 @@
+package dataStructureAlgorithmQuestions;
+
+import java.util.Scanner;
+
+public class PrintElementsLinkedList {
+
+    /*
+     * Hacker Rank Algorithm Question - Print the Elements of a Linked List Solution
+     *
+     * Solved Date : 2019-09-13
+     * Author : TK Lee
+     *
+     * Source : https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem
+     */
+
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    // Complete the printLinkedList function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static void printLinkedList(SinglyLinkedListNode head) {
+
+        System.out.println(head.data);
+        head = head.next;
+
+        if (head != null){
+            printLinkedList(head);
+        }
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < llistCount; i++) {
+            int llistItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            llist.insertNode(llistItem);
+        }
+
+        printLinkedList(llist.head);
+
+        scanner.close();
+    }
+
+}
