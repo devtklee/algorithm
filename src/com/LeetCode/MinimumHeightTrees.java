a3034305461d0ddde2317586b1852d66e8e0b1d9
TK Lee <devtklee@gmail.com>
2020-11-04 23:34:57 -0500

    November LeetCoding Challenge Q) Minimum Height Trees Question - In progress.

---
+++ src/com/LeetCode/MinimumHeightTrees.java
@@ -0,0 +1,56 @@
+package com.LeetCode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MinimumHeightTrees {
+    /*
+
+    LeetCode Algorithm Question - Minimum Height Trees Solution
+    Solved Date : 2020-11-04
+    Author : TK Lee
+
+    Source : https://leetcode.com/explore/challenge/card/november-leetcoding-challenge/564/week-1-november-1st-november-7th/3519/
+
+    Question Description :
+    A tree is an undirected graph in which any two vertices are connected by exactly one path. In other words, any connected graph without simple cycles is a tree.
+    Given a tree of n nodes labelled from 0 to n - 1, and an array of n - 1 edges where edges[i] = [ai, bi] indicates that there is an undirected edge between the two nodes ai and bi in the tree,
+     you can choose any node of the tree as the root. When you select a node x as the root, the result tree has height h. Among all possible rooted trees, those with minimum height (i.e. min(h))  are called minimum height trees (MHTs).
+    Return a list of all MHTs' root labels. You can return the answer in any order.
+    he height of a rooted tree is the number of edges on the longest downward path between the root and a leaf.
+
+    Example 1:
+    Input: n = 4, edges = [[1,0],[1,2],[1,3]]
+    Output: [1]
+    Explanation: As shown, the height of the tree is 1 when the root is the node with label 1 which is the only MHT.
+
+    Example 2:
+    Input: n = 6, edges = [[3,0],[3,1],[3,2],[3,4],[5,4]]
+    Output: [3,4]
+    Example 3:
+
+    Input: n = 1, edges = []
+    Output: [0]
+    Example 4:
+
+    Input: n = 2, edges = [[0,1]]
+    Output: [0,1]
+     */
+
+    public static List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> mht= new LinkedList<>();
+
+
+
+        return mht;
+    }
+
+    public static void main (String[] args){
+
+        int[][] edges1 = {{1,0}, {1,2}, {1,3}};
+
+        System.out.println(findMinHeightTrees(4, edges1));
+
+    }
+
+}
